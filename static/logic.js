3e540717594c923bf3485f5fdbc38f2e4f56afe2
cmorrow10 <114371722+cmorrow10@users.noreply.github.com>
2023-07-27 15:59:16 -0400

    Add files via upload

---
+++ static/logic.js
@@ -0,0 +1,89 @@
+// Store our API endpoint as queryUrl.
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+  // Send our earthquakes layer to the createMap function/
+ 
+
+    // Create tile layer
+   
+    var myMap = L.map("map", {
+      center: [
+        40.7, -94.5
+      ],
+      zoom: 3
+    })
+    var baseMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
+    baseMap.addTo(myMap);
+
+ // Perform a GET request to the query URL
+d3.json(queryUrl).then(function (data) {
+  // Console log the data retrieved 
+  console.log(data);
+  
+  // Once we get a response, send the data.features object to the createFeatures function.
+  createFeatures(data.features);
+});
+
+// Function to determine marker size
+function markerSize(magnitude) {
+  return magnitude * 2000;
+};
+
+// Function to determine marker color by depth
+function chooseColor(depth){
+  if (depth < 10) return "#00FF00";
+  else if (depth < 30) return "greenyellow";
+  else if (depth < 50) return "yellow";
+  else if (depth < 70) return "orange";
+  else if (depth < 90) return "orangered";
+  else return "#FF0000";
+}
+
+function createFeatures(earthquakeData) {
+
+  // Define a function that we want to run once for each feature in the features array.
+  // Give each feature a popup that describes the place and time of the earthquake.
+  function onEachFeature(feature, layer) {
+    layer.bindPopup(`<h3>Location: ${feature.properties.place}</h3><hr><p>Date: ${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
+  }
+
+  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+  // Run the onEachFeature function once for each piece of data in the array.
+    L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+
+    // Point to layer used to alter markers
+    pointToLayer: function(feature, latlng) {
+
+      // Determine the style of markers based on properties
+      var markers = {
+        radius: markerSize(feature.properties.mag),
+        fillColor: chooseColor(feature.geometry.coordinates[2]),
+        fillOpacity: 0.7,
+        color: "black",
+        stroke: true,
+        weight: 0.5
+      }
+      return L.circle(latlng,markers);
+    }
+  }).addTo(myMap);
+
+ // Add legend
+ var legend = L.control({position: "bottomright"});
+ legend.onAdd = function() {
+   var div = L.DomUtil.create("div", "info legend"),
+   depth = [-10, 10, 30, 50, 70, 90];
+
+
+   div.innerHTML += "<h3 style='text-align: center'>Depth</h3>"
+
+   for (var i = 0; i < depth.length; i++) {
+     div.innerHTML +=
+     '<i style="background:' + chooseColor(depth[i]) + '"></i> ' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+   console.log(chooseColor(depth[i]))}
+   return div;
+ };
+ legend.addTo(myMap)
+};
